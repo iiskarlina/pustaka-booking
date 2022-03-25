798d47962ab8d9c7a18e63cbc650dd8ffc31122e
iiskarlina <iiskarlina983@gmail.com>
2022-03-25 18:58:55 +0700

    menambahkan controllers,models,views

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h>Perkenalkan</h1>";
+        echo" Nama saya Iis Karlina
+              Saya tinggal di daerah Bekasi
+              Olahraga yang saya sukai adalah
+              Bulutangkis";
+
+    }
+}
