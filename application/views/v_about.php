6b954c3f9ac5aa126e5cb1fdce5c3cb176f748a9
iiskarlina <iiskarlina983@gmail.com>
2022-04-01 14:01:58 +0700

    menambahkan Web.php, v_about.php, v_header.php, v_footer.php, v_index.php

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<selection>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Iis</li>
+        <li>Nama Belakang : Karlina</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Green Park Bekasi Barat</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Ciamis</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Renang</li>
+    </ul>
+</selection> 
